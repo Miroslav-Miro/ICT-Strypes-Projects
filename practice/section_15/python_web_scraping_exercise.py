987b172e771858b37fb6fa94974cac6c3ed8cbd9
Miroslav-Miro <miroslavmazalov@gmail.com>
2025-04-13 03:43:11 +0300

    Add new practice/section

---
+++ practice/section_15/python_web_scraping_exercise.py
@@ -0,0 +1,55 @@
+import requests
+import bs4
+from typing import List
+
+def take_quotes(url: str) -> List[str]:
+    """
+    Fetches all quotes from the given URL
+    Args:
+        url (str): The URL of the page to scrape.
+    Returns:
+        List[str]: A list of quote texts extracted from the page.
+    """
+    res = requests.get(url)
+    soup = bs4.BeautifulSoup(res.text, 'lxml')
+    
+    # Select all span elements with class 'text'
+    quotes = soup.select('.text')
+    
+    text_quotes = [quote.text for quote in quotes]
+    return text_quotes
+
+
+def take_head_tags(url: str) -> List[str]:
+    """
+    Fetches all tags from the given URL
+    Args:
+        url (str): The URL of the page to scrape.
+    Returns:
+        List[str]: A list of tags texts extracted from the page.
+    """
+    res = requests.get(url)
+    soup = bs4.BeautifulSoup(res.text, 'lxml')
+    
+    head_tags_container = soup.select_one('.col-md-4.tags-box')
+    
+    if not head_tags_container:
+      return []
+    
+    tags = head_tags_container.select('.tag')
+    
+    
+    text_tags = [tag.text for tag in tags]
+    return text_tags
+  
+working_path = 'https://quotes.toscrape.com/'
+  
+quotes = take_quotes(working_path)
+print ('This are the home page quotes:')
+print('\n'.join(quotes))
+
+print('\n')
+
+tags = take_head_tags(working_path)
+print('Here are the tags:')
+print('\n'.join(tags))
